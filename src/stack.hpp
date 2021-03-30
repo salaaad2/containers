5e6cc0f872fa912eb3c6df67ffd15db4b911c121
Filemon Moenne-loccoz <fmoenne-@z2r5p8.42lyon.fr>
2021-03-30 14:51:35 +0200

    start with stack

---
+++ src/stack.hpp
@@ -0,0 +1,14 @@
+#ifndef FT_STACK_H
+#define FT_STACK_H
+
+namespace ft {
+    template <typename T>
+    class stack {
+        private :
+        public :
+            stack<T>() {}
+            ~stack<T>() {}
+    };
+}
+
+#endif // FT_STACK_H
