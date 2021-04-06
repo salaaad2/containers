111a83827048bf9ffc2d1a0cac1ea20e7b48a472
Filemon Moenne-loccoz <fmoenne-@z2r4p8.42lyon.fr>
2021-04-06 15:53:35 +0200

    add copy constructor and equals overloading

--- src/vector.hpp
+++ src/vector.hpp
@@ -13,25 +13,40 @@ namespace ft {
                 _size = 0;
                 _capacity = 8;
                 _elements = new value_type[_capacity];
+                return ;
             }
 
             explicit vector (size_type n,
                              const value_type& val = value_type()) {
-                _size = n * 2;
-                _capacity = 256; // TODO allocate 8 16 32 64
+                _size = n;
+                _capacity = 8; // TODO allocate 8 16 32 64
+                while (_capacity < n)
+                    _capacity *= 2;
                 _elements = new value_type[_capacity];
                 for (size_type i = 0; i < _size; i++) { _elements[i] = val; }
+                return ;
             }
 
-            // template <class InputIterator>
-            // vector (InputIterator first, InputIterator last) {
-            //     // TODO
-            // }
+            template <class InputIterator>
+            vector (InputIterator first, InputIterator last) {
+                // TODO
+            }
 
-            // vector (const vector &x) {
-            //     // TODO
-            // }
-            //
+            vector (const vector &x) {
+                *this = x;
+                return ;
+            }
+
+            vector & operator=(vector const & rhs) {
+                _capacity = rhs.capacity();
+                _size = rhs.size();
+                for (size_type i = 0; i < _size; i++ ) {_elements[i] = rhs[i];}
+                return ;
+            }
+
+            value_type & operator[](size_type n) {
+                return (_elements[n]);
+            }
 
             ~vector() {};
 
