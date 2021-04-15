108451ba9009a2bc6ec605e38c73a4b60dad6b9d
Filemon Moenne-loccoz <fmoenne-@z4r8p3.42lyon.fr>
2021-04-15 15:30:48 +0200

    qwe

---
+++ src/stack.hpp
@@ -0,0 +1,59 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "vector.hpp"
+
+namespace ft {
+    template <class T, class Container = vector<T> > class stack {
+        public :
+//
+// typedefs
+//
+            typedef T value_type;
+            typedef Container container_type;
+            typedef size_t size_type;
+
+//
+// init
+//
+            explicit stack (const container_type& ctnr = container_type()) {
+                _cont = ctnr;
+            }
+
+//
+// elements access
+//
+
+            void push (const value_type& val) {
+                _cont.push_back(val);
+            }
+
+            void pop () {
+                _cont.pop_back(val);
+            }
+
+            value_type & top() {
+                return (_cont.back());
+            }
+
+            const value_type & top() const  {
+                return (_cont.back());
+            }
+//
+// capacity
+//
+            bool empty() {
+                return (_cont.empty());
+            }
+
+            size_type size() {
+                return (_cont.size());
+            }
+
+        private :
+            vector<value_type> _cont;
+
+    };
+};
+
+#endif // STACK_H
