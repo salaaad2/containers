86dac3a1b1a271f5b8af026c6ba6b4aca12657f5
salaaad2 <salaaad2@gitlab.com>
2021-04-26 20:38:43 +0200

    sounds about right

--- inc/map.hpp
+++ inc/map.hpp
@@ -23,10 +23,10 @@ namespace ft {
             typedef const value_type&                        const_reference;
             typedef value_type*                              pointer;
             typedef const value_type*                        const_pointer;
-            typedef map_iterator<value_type>       iterator;
-            typedef const map_iterator<value_type> const_iterator;
+            typedef map_iterator<value_type>                 iterator;
+            typedef const map_iterator<value_type>           const_iterator;
             typedef size_t                                   size_type;
-            typedef s_node<value_type>             t_node;
+            typedef s_node<value_type>                       t_node;
 //
 // constructor/destructors
 //
@@ -45,12 +45,31 @@ namespace ft {
 // content modifiers
 //
 
-            std::pair<iterator, bool> insert (const value_type &val) {
+            std::pair<iterator, bool> insert (const_reference &val) {
                 t_node * tn = _create_node(val);
-                iterator itmp;
+                t_node * nu = _head;
+                iterator itmp = begin();
                 key_compare comp;
 
-                (void)tn;
+                if (_size == 0) {
+                    _head = tn;
+                }
+                else if (_key_dup(val)) {
+                    while ((*itmp).first != val.first) {
+                        itmp++;
+                    }
+                    return std::pair<iterator, bool>(itmp, false);
+                }
+                else {
+                    if ((comp(*itmp).first, val.first)) {
+                        nu = nu->right;
+                    }
+                    else {
+                        nu->left = tn;
+                        tn->parent = nu;
+                    }
+                }
+                itmp.setPtr(tn);
                 return std::pair<iterator, bool>(itmp, true);
             }
 
@@ -63,13 +82,36 @@ namespace ft {
                 return const_iterator(_head);
             }
 
-            t_node * _create_node(const value_type &val) {
+            iterator end() {
+                iterator ite = begin();
+                t_node * tmp = _head;
+
+                while (tmp->right) {
+                    tmp = tmp->right;
+                    ite++;
+                }
+                ite.setPtr(tmp);
+                return (ite);
+            }
+
+            t_node * _create_node(const_reference val) {
                 t_node * tmp = new t_node(val);
 
                 tmp->parent = NULL;
                 tmp->left = NULL;
                 tmp->right = NULL;
                 return (tmp);
+            }
+
+            bool _key_dup(const_reference val) {
+                iterator it = begin();
+
+                while (it != end()) {
+                    if ((*it).first == val.first)
+                    {return (true);}
+                    it++;
+                }
+                return (false);
             }
 
         private :
