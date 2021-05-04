75c13c421596bef5cf1c69e917dee960656be70d
salaaad2 <salaaad2@gitlab.com>
2021-05-05 01:22:52 +0200

    decapitation

--- inc/map.hpp
+++ inc/map.hpp
@@ -163,7 +163,7 @@ namespace ft {
                         tmp->left = NULL;
                     }
                 }
-                else if (t->right != NULL && t->left != NULL) { // children
+                else if (t->right != NULL && t->left != NULL && t->parent != NULL) { // children
                     t_node * ch = _get_child(t);
 
                     ch->parent->left = NULL;
@@ -171,6 +171,12 @@ namespace ft {
                     ch->left = t->left;
                     ch->right = t->right;
                     ch->parent = t->parent;
+                }
+                else if (t->right != NULL && t->left != NULL) { // decapitation
+                    t_node * ch = _get_child(t);
+
+                    t->data = ch->data;
+                    d = ch;
                 }
                 delete d;
             }
